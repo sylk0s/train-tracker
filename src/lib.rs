bb90ab94a9378904cdc7929fc79314bfd4294e84
sylkos <sylkos49@gmail.com>
2022-10-18 01:04:13 -0400

    fixed reading the vector from json

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ use serde_json::Value;
 use std::fs::File;
 use std::thread;
 
+const FILE_PATH: &str = "lines.json";
+
 pub async fn run() -> Result<(), Box<dyn Error>> {
     let client: Client = Config::new()
         .set_base_url(Url::parse("https://api-v3.mbta.com")?)
@@ -16,27 +18,42 @@ pub async fn run() -> Result<(), Box<dyn Error>> {
     let mut count = 0;
 
     //loop {
-        let mut res = client.get("/vehicles?filter[id]=G-10121,O-54731841,G-10063,R-54731200,B-5473183C,G-10026,G-10039").await?;
-        let result = &res.body_json::<Value>().await?;
-        /*
-         * need to figure out how to make rust trust that this will always be a vector
-        for train in result["data"] {
-            write_file_end(match_id_to_file(train["id"]));
+        let mut res = client.get("/vehicles?filter[route_type]=0,1").await?;
+        let result = &mut res.body_json::<Value>().await?;
+        // need to figure out how to make rust trust that this will always be a vector
+        for train in result["data"].as_array_mut().expect("aaa") {
+            let index = match train["relationships"]["route"]["data"]["id"] {
+                "Green-A" => 0,
+                "Green-B" => 1,
+                "Green-C" => 2,
+                "Green-D" => 3,
+                "Green-E" => 4,
+                "Red" => 5,
+                "Orange" => 6,
+                "Mattapan" => 7,
+                "Blue" => 8,
+            };
+
+
+            //println!("train: {:?}", train);
+            //let str = "";
+            //write_file_end(0 , &str);
         }
-        */
         count += 1;
         println!("Updated {} times", count);
-        thread::sleep(Duration::from_secs(15));
+        //thread::sleep(Duration::from_secs(15));
     //}
 
-    println!("the first train is {}", result["data"][0]["id"]);
+    //println!("the first train is {}", result["data"][0]["id"]);
+    //println!("{}", result);
     Ok(())
 }
 
-fn match_id_to_file(id: String) {
+// Matches train ID to an element in the json list
+fn match_id(id: String) -> u8 {
     unimplemented!();
 }
 
-fn write_file_end(file: File, data: String) {
+fn write_file_end(index: u8, data: &str) {
     unimplemented!();
 }
